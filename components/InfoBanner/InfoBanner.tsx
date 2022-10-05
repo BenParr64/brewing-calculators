7b73ddb09823dec6ba7d0078c9dbeb908d2cb2be
Ben Parr <BenParr@tfl.gov.uk>
2022-10-05 11:43:59 +0100

    banner message

--- components/InfoBanner/InfoBanner.tsx
+++ components/InfoBanner/InfoBanner.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styles from './style.module.css';
 
-const BannerMessage = "Kegthat Calculators Site"
+const BannerMessage = <>Return to <a href="https://kegthat.com/">Shop</a></>
 
 const InfoBanner = () => {
   return (
